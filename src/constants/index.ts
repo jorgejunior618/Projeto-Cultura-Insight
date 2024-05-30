178f6c53499e080e5b8da55dd16cbe73e378de85
jorgejunior618 <jorgejunior618@gmail.com>
2024-05-30 11:09:10 -0300

    Inicializando o projeto con NextJS

---
+++ src/constants/index.ts
@@ -0,0 +1,29 @@
+export const containers = {
+  viewContainer: 'viewContainer'
+}
+export const fontSizes = {
+  small: 'font-small',
+  regular: 'font-regular',
+  big: 'font-big',
+  xtraBig: 'font-xtrabig'
+}
+export const fontWeights = {
+  light: 'weigth-light',
+  regular: 'weigth-regular',
+  bold: 'weigth-bold',
+  xtraBold: 'weigth-xtrabold'
+}
+export const colors = {
+  mainBackground: '#0A0F32',
+  secBackground: '#060A20',
+  primary: '#0A829B',
+  red: '#BF1919',
+  green: '#00AA44'
+}
+export const lightColors = {
+  mainBackground: '#9DC4E0',
+  secBackground: '#85A8CE',
+  primary: '#0A829B',
+  red: '#BF1919',
+  green: '#00AA44'
+}
