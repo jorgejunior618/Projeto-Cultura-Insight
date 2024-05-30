8fac3efe0b586890c523ef03ed638175a9a0fde2
jorgejunior618 <jorgejunior618@gmail.com>
2024-05-30 17:58:12 -0300

    componentização do cabeçalho

---
+++ src/components/header/index.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useAppDispatch } from "@/src/hooks";
+import { useRouter } from "next/navigation";
+import { HeaderContainer, LoginContainer, LogoutContainer } from "./styled";
+import { LogoutOutlined } from "@ant-design/icons";
+import { colors, fontSizes, fontWeights } from "@/src/constants";
+
+type HeaderProps = {
+  logged: boolean;
+};
+
+export default function Header({logged}: HeaderProps) {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    // await dispatch(logout());
+    try {
+      router.push('/login');
+    } catch (erro) {
+      console.log({erro});
+      
+    }
+  }
+
+  return (
+    <HeaderContainer>
+      <img src="/images/logo-insight-completo.jpg" alt="Logotipo Insight" width={190}/>
+      {logged
+        ? <LogoutContainer><LogoutOutlined/></LogoutContainer>
+        : <LoginContainer className={fontSizes.medium + ' ' + fontWeights.bold}>Login</LoginContainer>
+      }
+    </HeaderContainer>
+  );
+}
---
+++ src/components/header/styled.ts
@@ -0,0 +1,43 @@
+import { colors } from "@/src/constants";
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 110px;
+
+  img {
+    border-radius: 5px;
+  }
+`;
+
+export const LogoutContainer = styled.div`
+  cursor: pointer;
+  padding: 15px 20px;
+  border-radius: 8px;
+  transition: background-color .4s;
+
+  &:hover {
+    background-color: ${colors.red}40;
+  }
+  span, span svg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    fill: ${colors.red};
+  }
+`;
+export const LoginContainer = styled.div`
+  cursor: pointer;
+  padding: 15px 20px;
+  border-radius: 8px;
+  transition: background-color .4s;
+  color: ${colors.green};
+
+  &:hover {
+    background-color: ${colors.green}40;
+  }
+`;
