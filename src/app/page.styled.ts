4d9d595d569190209ac040b7f3dad4ce8a3d3079
jorgejunior618 <jorgejunior618@gmail.com>
2024-05-30 23:34:32 -0300

    criação da pagina de listagem de fornecedores

--- src/app/page.styled.ts
+++ src/app/page.styled.ts
@@ -1,4 +1,49 @@
+import { colors } from "@/constants";
 import styled from "styled-components";
 
-export const Coisa = styled.main`
+export const HomeContainer = styled.section`
+  border-top: 1px solid white;
+  padding-top: 20px;
+
+  li {
+    h2 {
+      align-items: center;
+    }
+    p {
+      margin: 6px 0 4px;
+    }
+    .options {
+      cursor: pointer;
+      display: flex;
+      width: fit-content;
+      padding: 8px;
+      border-radius: 8px;
+      border: 3px solid ${colors.mainBackground};
+
+      span svg {
+        font-size: 1.5rem;
+      }
+    }
+  }
+`;
+
+export const DropdownOption = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  span svg {
+    margin-left: 6px;
+  }
+  &.editOption {
+    color: ${colors.primary};
+    span svg {
+      fill: ${colors.primary};
+    }
+  }
+  &.deleteOption {
+    color: ${colors.red};
+    span svg {
+      fill: ${colors.red};
+    }
+  }
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
 "use client"
 
+import { fontSizes, fontWeights } from "@/constants";
+import { DropdownOption, HomeContainer } from "./page.styled";
+import Card from "@/components/card";
+import { DeleteOutlined, EditOutlined, MoreOutlined } from "@ant-design/icons";
+
+import mockData from '@/mock.json';
+import { Dropdown } from "antd";
+
 export default function Home() {
   return (
-    <></>
+    <HomeContainer>
+    <h1 className={fontSizes.big}>Seus Fornecedores</h1>
+    {
+      mockData.fornecedores.map(fornecedor => (
+      <Card>
+        <section>
+          <h2 className={fontSizes.medium + ' ' + fontWeights.bold}>
+            {fornecedor.name + ' '}
+            <span className={fontSizes.regular + ' ' + fontWeights.regular}>(nome fantasia: {fornecedor.alternativeName})</span>
+          </h2>
+          <p>CNPJ: {fornecedor.cnpj}</p>
+          <p>{fornecedor.address.line_one}</p>
+          <p>{fornecedor.address.line_two || null}</p>
+          <p>{fornecedor.address.state} - {fornecedor.address.country}</p>
+        </section>
+          <Dropdown
+            trigger={['click']}
+            menu={{ items: [
+            {
+              label: (<DropdownOption className="editOption">Editar <EditOutlined /></DropdownOption>),
+              key: '0'
+            },
+            {
+              label: (<DropdownOption className="deleteOption">Excluir <DeleteOutlined /></DropdownOption>),
+              key: '1'
+            }
+          ]}}>
+            <section className="options">
+            <MoreOutlined />
+            </section>
+          </Dropdown>
+      </Card>
+      ))
+    }
+    </HomeContainer>
   );
 }
