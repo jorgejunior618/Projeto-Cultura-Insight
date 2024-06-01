f2fed1ca03903ec08d71b73c89325ff4472378a6
jorgejunior618 <jorgejunior618@gmail.com>
2024-06-01 14:00:34 -0300

    configuração de middleware para capturar as ações de navegação

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest } from "next/server";
+import { updateSession } from "@/utils/sessionManager";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+export const config = {
+  matcher: [
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+ 
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      has: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+ 
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      has: [{ type: 'header', key: 'x-present' }],
+      missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
+    },
+  ]
+};
