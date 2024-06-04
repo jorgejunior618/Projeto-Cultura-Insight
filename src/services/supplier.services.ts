65e32a30f609c9c30fb9340be97c3f973442c035
jorgejunior618 <jorgejunior618@gmail.com>
2024-06-04 14:03:57 -0300

    modificando a base de dados para arquivo local

--- src/services/supplier.services.ts
+++ src/services/supplier.services.ts
@@ -1,67 +1,63 @@
 import { SupplierType } from "@/redux/reduxTypes";
-
 import mockData from '@/mock.json';
+import { getSuppliersFile, setSuppliersFile } from "@/utils/fileHandler";
 
-function getSuppliers(): SupplierType[] {
-  const suppliers = localStorage.getItem("dev-insightlab-suppliers")
+async function getSuppliers(): Promise<SupplierType[]> {
+  const suppliers = await getSuppliersFile();
   if (!suppliers) {
-    localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(mockData.suppliers));
     return mockData.suppliers;
   }
-  return JSON.parse(suppliers);
+  return suppliers;
 }
-function getSupplier(supplierID: string): SupplierType | undefined {
-  const suppliers = localStorage.getItem("dev-insightlab-suppliers");
+async function getSupplier(supplierID: string): Promise<SupplierType | undefined> {
+  const suppliers = await getSuppliersFile();
   if (!suppliers) {
-    localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(mockData.suppliers));
     return mockData.suppliers.find(sup => sup.cnpj === supplierID);
   }
-  const suppliersArr: SupplierType[] = JSON.parse(suppliers);
-
-  return suppliersArr.find(sup => sup.cnpj === supplierID);
+  return suppliers.find(sup => sup.cnpj === supplierID);
 }
 
-function createSupplier(supplier: SupplierType): SupplierType[] {
-  const currentSuppliers = localStorage.getItem("dev-insightlab-suppliers")
+async function createSupplier(supplier: SupplierType): Promise<SupplierType[]> {
+  const currentSuppliers = await getSuppliersFile();
   if (!currentSuppliers) {
-    localStorage.setItem("dev-insightlab-suppliers", JSON.stringify([...mockData.suppliers, supplier]));
-    return [...mockData.suppliers, supplier];
+  setSuppliersFile([...mockData.suppliers, supplier]);
+  return [...mockData.suppliers, supplier];
   }
   
-  const suppliers = [...JSON.parse(currentSuppliers), supplier];
-  localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(suppliers));
+  const suppliers = [...currentSuppliers, supplier];
+  setSuppliersFile(suppliers);
   return suppliers;
 }
 
-function updateSupplier(supplier: SupplierType): SupplierType[] {
-  const currentSuppliers = localStorage.getItem("dev-insightlab-suppliers")
+async function updateSupplier(supplier: SupplierType): Promise<SupplierType[]> {
+  const currentSuppliers = await getSuppliersFile();
   if (!currentSuppliers) {
     const suppliersArray: SupplierType[] = mockData.suppliers;
     const supIndex = suppliersArray.findIndex(sup => sup.cnpj === supplier.cnpj);
     const suppliers = [...suppliersArray.slice(0, supIndex), supplier, ...suppliersArray.slice(supIndex + 1)];
-    localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(suppliers));
+    setSuppliersFile(suppliers);
     return suppliers;
   }
 
-  const suppliersArray: SupplierType[] = JSON.parse(currentSuppliers);
+  const suppliersArray: SupplierType[] = currentSuppliers;
   const supIndex = suppliersArray.findIndex(sup => sup.cnpj === supplier.cnpj);
   const suppliers = [...suppliersArray.slice(0, supIndex), supplier, ...suppliersArray.slice(supIndex + 1)];
-  localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(suppliers));
+  setSuppliersFile(suppliers);
   return suppliers;
 }
 
-function deleteSupplier(supplierID: string): SupplierType[] {
+async function deleteSupplier(supplierID: string): Promise<SupplierType[]> {
   const currentSuppliers = localStorage.getItem("dev-insightlab-suppliers")
   if (!currentSuppliers) {
     const suppliersArray: SupplierType[] = mockData.suppliers;
     const suppliers = suppliersArray.filter(sup => sup.cnpj !== supplierID);
-    localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(suppliers));
+    setSuppliersFile(suppliers);
     return suppliers;
   }
 
   const suppliersArray: SupplierType[] = JSON.parse(currentSuppliers);
   const suppliers = suppliersArray.filter(sup => sup.cnpj !== supplierID);
-  localStorage.setItem("dev-insightlab-suppliers", JSON.stringify(suppliers));
+  setSuppliersFile(suppliers);
   return suppliers;
 }
 
