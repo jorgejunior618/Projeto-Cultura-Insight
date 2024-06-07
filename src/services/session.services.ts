b9046c479c50f7e006cc479661801721323f4f3b
jorgejunior618 <jorgejunior618@gmail.com>
2024-06-07 00:47:35 -0300

    testando erro em produção

--- src/services/session.services.ts
+++ src/services/session.services.ts
@@ -10,9 +10,19 @@ async function sleep(ms: number) {
 export async function login(username: string, password: string): Promise<UserSessionType> {
   await sleep(1500);
   const user: UserSessionType = { username, addSupplier: true };
-  if (username === 'user' && password === 'user') user.addSupplier = false;
-  else if (username !== 'admin' || password !== 'admin') throw new Error('Credenciais inválidas');
+  if (username === 'user' && password === 'user') {
+    console.log('if');
+    
+    user.addSupplier = false;
+  }
+  else if (username !== 'admin' || password !== 'admin') {
+    console.log('else if');
+    
+    throw new Error('Credenciais inválidas');
+    }
+  console.log('setSessionCookies');
   await setSessionCookies({user, logged: true});
+  console.log('return user');
   return user;
 }
 export async function logout(): Promise<boolean> {
