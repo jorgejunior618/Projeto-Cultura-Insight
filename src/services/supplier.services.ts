ff5b331d121a3fa9847b98af330051a08f9bc8ab
jorgejunior618 <jorgejunior618@gmail.com>
2024-06-07 01:23:03 -0300

    utilizando base de dados sem persistencia local

--- src/services/supplier.services.ts
+++ src/services/supplier.services.ts
@@ -1,38 +1,23 @@
 import { SupplierType } from "@/redux/reduxTypes";
-import mockData from '@/mock.json';
-import { getSuppliersFile, setSuppliersFile } from "@/utils/fileHandler";
+
+import data from "@/mock";
+import { sleep } from "@/utils/functions";
 
 async function getSuppliers(): Promise<SupplierType[]> {
-  const suppliers = await getSuppliersFile();
-  if (!suppliers) {
-    return mockData.suppliers;
-  }
+  const suppliers = data.suppliers;
+  await sleep(750);
   return suppliers;
 }
 async function getSupplier(supplierID: string): Promise<SupplierType | undefined> {
-  const suppliers = await getSuppliersFile();
-  if (!suppliers) {
-    return mockData.suppliers.find(sup => sup.cnpj === supplierID);
-  }
+  const suppliers = data.suppliers;
+  await sleep(750);
   return suppliers.find(sup => sup.cnpj === supplierID);
 }
-
+  
 async function createSupplier(supplier: SupplierType): Promise<{success: boolean; message: string}> {
-  const currentSuppliers = await getSuppliersFile();
-  if (!currentSuppliers) {
-    if (mockData.suppliers.find(sup => sup.cnpj === supplier.cnpj)) {
-      return {
-        success: false,
-        message: "Este CNPJ ja está cadastrado em um fornecedor"
-      };
-    }
-
-    setSuppliersFile([...mockData.suppliers, supplier]);
-    return {
-      success: true,
-      message: "Fornecedor cadastrado com sucesso"
-    };
-  }
+  const currentSuppliers = data.suppliers;
+  await sleep(750);
+  
   if (currentSuppliers.find(sup => sup.cnpj === supplier.cnpj)) {
     return {
       success: false,
@@ -40,44 +25,32 @@ async function createSupplier(supplier: SupplierType): Promise<{success: boolean
     };
   }
   
-  const suppliers = [...currentSuppliers, supplier];
-  setSuppliersFile(suppliers);
+  data.suppliers.push(supplier);
+  await sleep(750);
   return {
     success: true,
     message: "Fornecedor cadastrado com sucesso"
   };
 }
 
-async function updateSupplier(supplier: SupplierType): Promise<SupplierType[]> {
-  const currentSuppliers = await getSuppliersFile();
-  if (!currentSuppliers) {
-    const suppliersArray: SupplierType[] = mockData.suppliers;
-    const supIndex = suppliersArray.findIndex(sup => sup.cnpj === supplier.cnpj);
-    const suppliers = [...suppliersArray.slice(0, supIndex), supplier, ...suppliersArray.slice(supIndex + 1)];
-    setSuppliersFile(suppliers);
-    return suppliers;
-  }
+async function updateSupplier(supplier: SupplierType): Promise<SupplierType> {
+  const currentSuppliers = data.suppliers;
+  await sleep(750);
 
   const suppliersArray: SupplierType[] = currentSuppliers;
   const supIndex = suppliersArray.findIndex(sup => sup.cnpj === supplier.cnpj);
   const suppliers = [...suppliersArray.slice(0, supIndex), supplier, ...suppliersArray.slice(supIndex + 1)];
-  setSuppliersFile(suppliers);
-  return suppliers;
+  data.suppliers = suppliers;
+
+  return supplier;
 }
 
-async function deleteSupplier(supplierID: string): Promise<SupplierType[]> {
-  const currentSuppliers = await getSuppliersFile();
-  if (!currentSuppliers) {
-    const suppliersArray: SupplierType[] = mockData.suppliers;
-    const suppliers = suppliersArray.filter(sup => sup.cnpj !== supplierID);
-    setSuppliersFile(suppliers);
-    return suppliers;
-  }
+async function deleteSupplier(supplierID: string): Promise<string> {
+  const currentSuppliers = data.suppliers;
+  await sleep(750);
 
-  const suppliersArray: SupplierType[] = currentSuppliers;
-  const suppliers = suppliersArray.filter(sup => sup.cnpj !== supplierID);
-  setSuppliersFile(suppliers);
-  return suppliers;
+  data.suppliers = currentSuppliers.filter(sup => sup.cnpj !== supplierID);
+  return supplierID;
 }
 
 const supplierServices = {
